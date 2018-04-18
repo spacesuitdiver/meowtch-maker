806f63de7c0130a58a1b5890eaf72a157f0f2948
Chris LeBlanc <spacesuitdiver@gmail.com>
2018-04-18 12:28:30 -0500

    Make it "cachink!"

--- components/capture-view.js
+++ components/capture-view.js
@@ -3,15 +3,57 @@ $(document).ready(function () {
   var viewOutlet = $('#capture-view-outlet');
 
   function render() {
-    var template = $('<div class="view capture-view text-center">');
-    template.append('<img src="images/logo.gif" alt="" class="logo">');
-    template.append('<h1 class="text-light">Meowtch Maker!</h1>');
-    template.append('<p class="text-light">Find your prrrrfect match.</p>');
+    var webcamComponent = renderWebcam();
 
-    var captureButton = $('<button class="btn btn-primary">Cachink!</button>');
-    template.append(captureButton);
+    var viewElement = $(`<div class="view capture-view text-center">`);
+    viewElement.append(webcamComponent.element);
+    viewElement.append(`<h1 class="text-light">Take Your Photo</h1>`);
+    viewElement.append(`<p class="text-light">Let's find you some matches!</p>`);
 
-    viewOutlet.append(template);
+    var captureButtonElement = $('<button class="btn btn-primary">Cachink!</button>');
+    captureButtonElement.click(function() {
+      var base64Image = webcamComponent.onCapture();
+      userCardImageElement.attr('src' , base64Image);
+    });
+    viewElement.append(captureButtonElement);
+
+    var comparisonElement = $('<div class="container mt-5">'); 
+    var comparisonRowElement = $('<div class="row justify-content-around">');
+    comparisonElement.append(comparisonRowElement);
+
+    var userCardElement = $('<div class="card mx-2 col-md-4 p-0">');
+    var userCardImageElement = $('<img class="card-img-top">');
+    var userCardBodyElement = $(`
+      <div class="card-body">
+        <h5 class"card-title">You!</h5>
+        <div class="card-text">
+          <ul>
+            <li>Age: 54</li>
+            <li>Weight: 200lbs</li>
+          </ul>
+        </div>
+    `);
+    userCardElement.append(userCardImageElement, userCardBodyElement);
+
+    var matchCardElement = $('<div class="card mx-2 col-md-4 p-0">');
+    var matchCardImageElement = $('<img class="card-img-top">');
+    var matchCardBodyElement = $(`
+      <div class="card-body">
+        <h5 class"card-title">Fido</h5>
+        <div class="card-text">
+          <ul>
+            <li>Age: 54</li>
+            <li>Weight: 200lbs</li>
+          </ul>
+        </div>
+    `);
+    matchCardElement.append(matchCardImageElement, matchCardBodyElement);
+
+    comparisonRowElement.append(userCardElement, matchCardElement);
+
+    viewElement.append(comparisonElement);
+
+    viewOutlet.append(viewElement);
   }
 
   // an easy "global" way to handle navigation state across views
